40c3484f57069cd9e805af6f877f748fc8e9fd21
LemonHum4n <lemonhuman2007@gmail.com>
2025-10-20 15:38:23 -0300

    aaaa

--- RamonBarrosLuco/mobile/RamonBarrosLuco/src/app/services/vehiculo-service.ts
+++ RamonBarrosLuco/mobile/RamonBarrosLuco/src/app/services/vehiculo-service.ts
@@ -25,6 +25,21 @@ export class VehiculoService {
     return  response;
   }
 
+  async editarVehiculo(body:Auto){
+    const response = await lastValueFrom(this.http.put<any>(this.apiUrl + "api/autos",body));
+    return  response;
+  }
+
+  async eliminarVehiculo(patente:String){
+    const response = await lastValueFrom(this.http.delete<any>(this.apiUrl + "api/autos" + patente));
+    return  response;
+  }
+
+  async buscarVehiculo(patente:String){
+    const response = await lastValueFrom(this.http.get<any>(this.apiUrl + "api/autos" + patente));
+    return  response;
+  }
+
   
 }
 
